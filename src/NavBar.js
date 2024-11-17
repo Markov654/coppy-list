72613f5718eaa7a743212d8ec0d61f563238c77c
Markov654 <iliq_markov@abv.bg>
2024-11-17 22:44:09 +0200

    try to set up responsive navBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom';
 import logo from "./car.png"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 
 const Navbar = () => {
     return (
-        <div className='container'>
-        <img src={logo} width={150} height={150} />
-        <nav className='nav'>
+        <Container>
+        <Row src={logo} width={150} height={150} />
+        <Col className='nav'>
             <ul>
-                <li><Link to="/">За нас</Link></li>
-                <li><Link to="/about">Автопарк</Link></li>
-                <li><Link to="/projects">Често задавани въпроси</Link></li>
-                <li><Link to="/contact">Контакти</Link></li>
+                <Col><Link to="/">За нас</Link></Col>
+                <Col><Link to="/about">Автопарк</Link></Col>
+                <Col><Link to="/projects">Често задавани въпроси</Link></Col>
+                <Col><Link to="/contact">Контакти</Link></Col>
             </ul>
-        </nav>
+        </Col>
         <button className='button'> Виж колите</button>
-        </div>
+        </Container>
     );
 }
  
