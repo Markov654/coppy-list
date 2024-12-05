e2f2611d9d4fe95f8c93792848086693eb8c8931
Markov654 <iliq_markov@abv.bg>
2024-12-05 17:15:02 +0200

    chnache color chanche margin botton

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import  {  Container } from 'react-bootstrap';
 const App = () => {
     return (
         <>
-        <Container>
+        
             <Navbar />
-            </Container>
+            
             <Outlet />
         </>
 
