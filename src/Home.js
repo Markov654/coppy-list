c5ec59ef967b56f352dc772260897f1ab099a0a2
Markov654 <iliq_markov@abv.bg>
2024-12-05 18:44:47 +0200

    add nex text

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,24 @@ const Home = () => {
 
     return (<div className="container">
         <div className="row">
-            <div className="col-md-12 mql" >
+            <div className="col-md-12 head" >
             <h1 >Най-доброто пътуване</h1>
             <h2 >Kоли под наем</h2>
             </div>
+            
+        </div>
+        <div className="row">
+        <div className="col-md-12 mql">
+
+</div>
+        </div>
+
+        <div className="row">
+        <div className="col-md-12 mql">
+        <p> Ние предлагаме коли в отлично тежническо състояние и напълно 
+            обслужени на кокурентно ниски цени.
+        </p>
+</div>
         </div>
         
        
