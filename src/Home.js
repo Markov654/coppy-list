971afc49d8f0e3fcebd582580fa186280ea58570
Markov654 <iliq_markov@abv.bg>
2024-12-05 18:29:26 +0200

    add style

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
     return (<div className="container">
         <div className="row">
             <div className="col-md-12 mql" >
-            <h1 className="h1">Най-доброто пътуване</h1>
-            <h2 className="h2">Kоли под наем</h2>
+            <h1 >Най-доброто пътуване</h1>
+            <h2 >Kоли под наем</h2>
             </div>
         </div>
         
